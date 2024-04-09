5ec2aa245aa40f0d8835d44872a70ae462112063
AlmightyMatheus <matheusbrandaomatheus@gmail.com>
2024-04-09 10:47:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, "");
+    str = str.toLowerCase();
+    let length = str.length;
+    for (let i = 0; i < length / 2; i++) {
+        if (str[i] !== str[length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } else if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return [min, max];
 }
